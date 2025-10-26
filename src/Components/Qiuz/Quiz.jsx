c0c2c57bcb1bfc0b6c0c8cf347a365a9550a021f
Mithun C M <mithunpraju@gmail.com>
2025-10-26 18:06:27 +0530

    adding css files

--- src/Components/Qiuz/Quiz.jsx
+++ src/Components/Qiuz/Quiz.jsx
@@ -8,6 +8,14 @@ const Quiz = () => {
         <h1>Quiz App</h1>
         <hr />
         <h2>Which device is required for the Internet connection </h2>
+        <ul>
+            <li>Modern</li>
+            <li>Router</li>
+            <li>LAN Cable</li>
+            <li>PenDrive</li>
+        </ul>
+        <button>Next</button>
+        <div className="index">1 of 5 questions</div>
     </div>
     </>
   )
