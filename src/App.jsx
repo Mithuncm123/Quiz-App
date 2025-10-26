131f0c5709afb1594cbc954e8db8df3cfc62a8ec
Mithun C M <mithunpraju@gmail.com>
2025-10-26 16:31:02 +0530

    Initaizing the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Quiz from './Components/Qiuz/Quiz'
+
+const App=()=> {
+
+  return (
+    <>
+      
+        <Quiz/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Qiuz/Quiz.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Quiz.css'
+
+const Quiz = () => {
+  return (
+    <>
+    <div className='container'>
+        <h1>Quiz App</h1>
+        <hr />
+        <h2>Which device is required for the Internet connection </h2>
+    </div>
+    </>
+  )
+}
+
+export default Quiz
