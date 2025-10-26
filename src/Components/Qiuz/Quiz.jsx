26efea819faca09e353de6159808520ac8b845fe
Mithun C M <mithunpraju@gmail.com>
2025-10-26 19:32:52 +0530

    Updated

--- src/Components/Qiuz/Quiz.jsx
+++ src/Components/Qiuz/Quiz.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
+import React,{useState, useRef} from 'react'
 import './Quiz.css'
+import { data } from '../../assets/data'
 
 const Quiz = () => {
+    let [index,setIndex]=useState(1);
+    let [question,setQuestion]=useState(data[index])
+
+    let [lock,setLock]=useState(false)
+    let option1=useRef(null)
+    let option2=useRef(null)
+    let option3=useRef(null)
+    let option4=useRef(null)
+
+    let option_array=[option1,option2,option3,option4]
+
+    const checkAns=(e,ans)=>{
+        if (lock===false){
+            if (question.ans===ans){
+                e.target.classList.add("Correct")
+                setLock(true)
+            }
+            else{
+                e.target.classList.add("Wrong")
+                setLock(true)
+                option_array[question.ans-1].current.classList.add("Correct")
+            }
+        }
+    }
+
   return (
     <>
     <div className='container'>
         <h1>Quiz App</h1>
         <hr />
-        <h2>Which device is required for the Internet connection </h2>
+        <h2>{index+1}. {question.question}</h2>
         <ul>
-            <li>Modern</li>
-            <li>Router</li>
-            <li>LAN Cable</li>
-            <li>PenDrive</li>
+            <li ref={option1} onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+            <li ref={option2} onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
+            <li ref={option3} onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+            <li ref={option4} onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
         </ul>
         <button>Next</button>
         <div className="index">1 of 5 questions</div>
---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data=[
+    {
+        question:"Which device is required for the Internet connection?",
+        option1:"Moderm",
+        option2:"Router",
+        option3:"LAN Cable",
+        option4:"Pen Drive",
+        ans:1,
+    },
+    {
+    question: "Which language is primarily used for web development?",
+    option1: "Python",
+    option2: "HTML",
+    option3: "C++",
+    option4: "Java",
+    ans: 2,
+},
+{
+    question: "What does CPU stand for in computers?",
+    option1: "Central Processing Unit",
+    option2: "Computer Primary Unit",
+    option3: "Central Peripheral Unit",
+    option4: "Control Processing Unit",
+    ans: 1,
+},
+{
+    question: "Which of these is a storage device?",
+    option1: "Monitor",
+    option2: "Keyboard",
+    option3: "Hard Disk",
+    option4: "Mouse",
+    ans: 3,
+},
+{
+    question: "Which of these is an input device?",
+    option1: "Printer",
+    option2: "Keyboard",
+    option3: "Speaker",
+    option4: "Monitor",
+    ans: 2,
+}
+]
